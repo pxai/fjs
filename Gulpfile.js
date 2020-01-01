f4fbf0397b1358c9b01bc7b3c8e6ccb1f1b64d6e
Pello Altadill <pello.altadill@gluckgames.com>
2020-01-01 11:26:04 +0100

    Working with watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,60 @@
-const { src, dest, parallel } = require('gulp');
-const pug = require('gulp-pug');
-const less = require('gulp-less');
+const { src, dest, parallel, series, watch } = require('gulp');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
+const del = require('del');
+const browsersync = require('browser-sync').create();
 
 function html() {
-  return src('client/templates/*.pug')
-    .pipe(pug())
-    .pipe(dest('build/html'))
+  return src('src/**/index.html')
+    .pipe(dest('dist'))
 }
 
 function css() {
-  return src('client/templates/*.less')
-    .pipe(less())
+  return src('src/**/css/*.css')
     .pipe(minifyCSS())
-    .pipe(dest('build/css'))
+    .pipe(dest('dist'))
 }
 
 function js() {
-  return src('client/javascript/*.js', { sourcemaps: true })
+  return src('src/app1/js/*.js', { sourcemaps: true, base: 'src/app1' })
     .pipe(concat('app.min.js'))
-    .pipe(dest('build/js', { sourcemaps: true }))
+    .pipe(dest('dist/app1/js', { sourcemaps: true }))
 }
+
+function clean() {
+    return del(["dist"]);
+}
+
+function browserSync(done) {
+    browsersync.init({
+      server: {
+        baseDir: "dist"
+      },
+      port: 3000
+    });
+    done();
+  }
+  
+  // BrowserSync Reload (callback)
+  function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+  }
+
+  function watchFiles() {
+    watch("./src/**/index.html", html);
+    watch("./src/**/css/*", css);
+    watch("./src/**/js/*", series(js));
+    watch(
+      [
+        "./dist/**/*",
+      ],
+      series(browserSyncReload)
+    );
+  }
 
 exports.js = js;
 exports.css = css;
 exports.html = html;
-exports.default = parallel(html, css, js);
+exports.build = series(clean, parallel(html, css, js));
+exports.default = parallel(watchFiles, browserSync);
