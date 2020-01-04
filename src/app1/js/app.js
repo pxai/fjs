9413bfceaac0ad399137fe397e9d2a4ca31d965d
Pello Altadill <pello.altadill@gluckgames.com>
2020-01-04 23:08:03 +0100

    Finally working, reordered

--- src/app1/js/app.js
+++ src/app1/js/app.js
@@ -1,12 +1,5 @@
-import h from "hyperscript";
-import hh from "hyperscript-helpers"
 
-const { div } = hh(h);
-
-function view (model) {
-    console.log("model: ", model);
-    return div(model.content)
-}
+import view from "./view";
 
 const rootNode = document.getElementById("app");
 console.log(rootNode,"Ea")
--- src/app1/js/view.js
+++ src/app1/js/view.js
@@ -1,4 +1,10 @@
-export default function hello () {
-    console.log("Hello There");
+import h from "hyperscript";
+import hh from "hyperscript-helpers"
+const { div } = hh(h);
+
+function view (model) {
+    console.log("model: ", model);
+    return div(model.content)
 }
 
+export default view;
