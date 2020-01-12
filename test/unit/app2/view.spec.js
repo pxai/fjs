e004c5e448b4fc8447ec59ff76877a0f1487710a
Pello Altadill <pello.altadill@gluckgames.com>
2020-01-12 01:08:15 +0100

    Added mocha/expect support

---
+++ test/unit/app2/view.spec.js
@@ -0,0 +1,9 @@
+import expect from "expect";
+import view from "../../../src/app2/js/view";
+
+
+describe("app2 view", () => {
+    it("works", () => {
+        expect(true).toBe(true);
+    });
+});
