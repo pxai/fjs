22b9fd666005349780fb927bb40fbd56f1621b9d
Pello Altadill <pello.altadill@gluckgames.com>
2020-03-08 18:34:11 +0100

    Improved IO

--- src/app4/js/init.js
+++ src/app4/js/init.js
@@ -1,25 +1,27 @@
 import { urlStream } from "./eventstreams";
 import { catchPokemon } from "./future";
-import { resultIO, pokemonIO } from "./io";
+import { notFoundIO, clearIO, pokemonIO } from "./io";
 import Either from "folktale/result";
 import { log, map, compose, mjoin } from "./utils";
 import * as R from "ramda";
 
 //const getPokemon = R.compose(catchPokemon, log)
+/*
 const validPokemon = function(pokemon) {
     console.log("Lets see: ", pokemon.name);
     return pokemon.name ? Either.Ok(pokemon) : Either.Error("Not a valid pokemon!");
   };
 
+
 const showIt = R.compose(
     mjoin,
     map(pokemonIO),
     validPokemon
-);
+);*/
 
 // IMPURE starts here:(
 // const catchPokemon = (url) => fetchUrl(url).fork(err => Either.Error(err), suc => Either.Ok(suc))
-const catchPokemonF = (url) => catchPokemon(url).fork(err => showIt(err).run(), suc => showIt(suc).run());
+const catchPokemonF = (url) => catchPokemon(url).fork(err => clearIO().run(), suc => pokemonIO(suc).run());
 // const catchPokemonLog = R.compose(catchPokemonF, log)
 // urlStream.onValue(R.compose(catchPokemonF, log));
 urlStream.onValue(catchPokemonF);
--- src/app4/js/io.js
+++ src/app4/js/io.js
@@ -2,19 +2,24 @@ import { IO } from "monet";
 import * as R from "ramda";
 
 const htmlIO = R.curry((selector, content) => IO(function() {
-  console.log("About to set: ", selector, content);
   return document.querySelector(selector).innerHTML = content;
 }));
 
 const pokemonIO = (content) => IO(function() {
-  console.log("About to set: ", content);
-  document.querySelector("#name").innerHTML = content.name;
-  document.querySelector("#base_experience").innerHTML = `Base experience: ${content.base_experience}`;
-  document.querySelector("#weight").innerHTML = `Weight: ${content.weight}`;
+  ["name", "base_experience", "weight"].forEach( element => {
+      document.querySelector(`#${element}`).innerHTML = `${element}: ${content[element]}`;
+  })
   document.querySelector("#image").src = content.sprites.front_default;
 });
 
+const clearIO = (content) => IO(function() {
+  ["name", "base_experience", "weight"].forEach( element => {
+    document.querySelector(`#${element}`).innerHTML = "";
+  })
+  document.querySelector("#image").src = "";
+});
 
-const resultIO = htmlIO("#result");
 
-export { htmlIO, resultIO, pokemonIO };
+const notFoundIO = htmlIO("#name");
+
+export { htmlIO, clearIO, pokemonIO };
