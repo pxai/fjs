276662b8d86a5fadceec4a05b4f72591cc3fd968
Pello Altadill <pello.altadill@gluckgames.com>
2020-03-08 18:07:36 +0100

    Project working

--- src/app4/js/init.js
+++ src/app4/js/init.js
@@ -1,6 +1,6 @@
 import { urlStream } from "./eventstreams";
 import { catchPokemon } from "./future";
-import { resultIO } from "./io";
+import { resultIO, pokemonIO } from "./io";
 import Either from "folktale/result";
 import { log, map, compose } from "./utils";
 import * as R from "ramda";
@@ -9,6 +9,7 @@ import * as R from "ramda";
 
 // IMPURE starts here:(
 // const catchPokemon = (url) => fetchUrl(url).fork(err => Either.Error(err), suc => Either.Ok(suc))
-const catchPokemonF = (url) => catchPokemon(url).fork(err => err, suc => log(suc))
-const catchPokemonLog = R.compose(catchPokemonF, log)
-urlStream.onValue(catchPokemonLog);
+const catchPokemonF = (url) => catchPokemon(url).fork(err => err, suc => pokemonIO(suc).run());
+// const catchPokemonLog = R.compose(catchPokemonF, log)
+// urlStream.onValue(R.compose(catchPokemonF, log));
+urlStream.onValue(catchPokemonF);
--- src/app4/js/io.js
+++ src/app4/js/io.js
@@ -1,11 +1,20 @@
 import { IO } from "monet";
 import * as R from "ramda";
-import { map } from "./utils";
 
 const htmlIO = R.curry((selector, content) => IO(function() {
+  console.log("About to set: ", selector, content);
   return document.querySelector(selector).innerHTML = content;
 }));
 
-const resultIO = htmlIO("result");
+const pokemonIO = (content) => IO(function() {
+  console.log("About to set: ", content);
+  document.querySelector("#name").innerHTML = content.name;
+  document.querySelector("#base_experience").innerHTML = `Base experience: ${content.base_experience}`;
+  document.querySelector("#weight").innerHTML = `Weight: ${content.weight}`;
+  document.querySelector("#image").src = content.sprites.front_default;
+});
 
-export { htmlIO, resultIO };
+
+const resultIO = htmlIO("#result");
+
+export { htmlIO, resultIO, pokemonIO };
