d83dba0ae53752602afe6c4df837b62dea74ba97
Pello Altadill <pello.altadill@gluckgames.com>
2020-03-08 18:19:17 +0100

    Adds a sample with either

--- src/app4/js/init.js
+++ src/app4/js/init.js
@@ -2,14 +2,24 @@ import { urlStream } from "./eventstreams";
 import { catchPokemon } from "./future";
 import { resultIO, pokemonIO } from "./io";
 import Either from "folktale/result";
-import { log, map, compose } from "./utils";
+import { log, map, compose, mjoin } from "./utils";
 import * as R from "ramda";
 
 //const getPokemon = R.compose(catchPokemon, log)
+const validPokemon = function(pokemon) {
+    console.log("Lets see: ", pokemon.name);
+    return pokemon.name ? Either.Ok(pokemon) : Either.Error("Not a valid pokemon!");
+  };
+
+const showIt = R.compose(
+    mjoin,
+    map(pokemonIO),
+    validPokemon
+);
 
 // IMPURE starts here:(
 // const catchPokemon = (url) => fetchUrl(url).fork(err => Either.Error(err), suc => Either.Ok(suc))
-const catchPokemonF = (url) => catchPokemon(url).fork(err => err, suc => pokemonIO(suc).run());
+const catchPokemonF = (url) => catchPokemon(url).fork(err => showIt(err).run(), suc => showIt(suc).run());
 // const catchPokemonLog = R.compose(catchPokemonF, log)
 // urlStream.onValue(R.compose(catchPokemonF, log));
 urlStream.onValue(catchPokemonF);
